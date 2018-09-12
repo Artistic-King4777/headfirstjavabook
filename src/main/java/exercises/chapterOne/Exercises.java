30f1aa57d41334498cabb3373b52c4b9f43dbc38
NightShadow3000 <RoyalFotoK@gmail.com>
2018-09-12 13:50:16 -0400

    headfirstjavabook!

---
+++ src/main/java/exercises/chapterOne/Exercises.java
@@ -0,0 +1,39 @@
+package exercises.chapterOne;
+
+public class Exercises {
+
+    /*
+    FIRST SuP::
+    which means a Sharpen ur Pencil exercise
+
+try to guess what each line of code is doing (answers will be under the exercise)
+
+int size = 27;                      | declares an integer variable named 'size' and give it the value 27 --- example
+String name = Fido;                 |
+Dog myDog = new Dog (name, size);   |
+x = size - 5;                       |
+if (x < 15) myDog .bark(8);         |
+-----------------------------------------------------
+while (x > 3) {                     |
+myDog.play ();                      |
+}                                   |
+-----------------------------------------------------
+int[] numList = {2,4,6,8};          |
+System.out.print("Hello");          |
+System.out.print("Dog: " + name);   |
+String num = "8";                   |
+int z = Integer.parseInt(num);      |
+-------------------------------------------------------------
+try {                               |
+readTheFile("myFile.txt");          |
+}                                   |
+catch(FileNotFoundException ex) {   |
+System.out.print("File not found.") |
+
+
+     */
+
+
+
+
+}
---
+++ src/main/java/notes/NotesOne.java
@@ -0,0 +1,30 @@
+package notes;
+
+public class NotesOne {
+
+    /*
+Java Book excercises and notes:
+----------------------------
+Key:
+
+Sharpen ur Pencil: SuP
+- DO ALL (no fully concrete answers)
+
+
+Running Shoe Symbol: RSS
+- Exercise: Mandatory DON'T SKIP
+
+Puzzle Logo: PuZZ
+- OPTIONAL twisty logic or crossword puzzles (for puzzlers ///dont be discouraged if you cant solve a puzzle)
+
+Code Examples: CoDEx
+- lean as possible so you don't have to sift through 200 lines of code for whats wrong
+- and so the part your trying to learn is clear and simple
+- MAY NOT EVEN BE COMPLETE: "that is YOUR assignment after you finish the book"
+
+
+
+
+
+     */
+}
